52edef895966d79eaa93cefc5f1bd51634b936d8
hrvstr <r.muhler@me.com>
2022-07-13 15:09:13 +0000

    Add the game function to play 5 rounds with user input

--- main.js
+++ main.js
@@ -15,65 +15,80 @@ function gamePlay(playerSelection, computerSelection) {
   // Transform case to allow case-insensitive user input
   playerSelection = playerSelection.toLowerCase();
 
-  // Log out picked options for debugging
-  console.log(`player: ${playerSelection}`);
-  console.log(`cpu: ${computerSelection}`);
+  // Check if playerSelection is valid gameOption
+  if (gameOptions.includes(playerSelection)) {
+    // Log out picked options for debugging
+    console.log(`player: ${playerSelection}`);
+    console.log(`cpu: ${computerSelection}`);
 
-  // ROCK
-  if (
-    playerSelection == gameOptions[0] &&
-    computerSelection == gameOptions[0]
-  ) {
-    return "Put!";
-  } else if (
-    playerSelection == gameOptions[0] &&
-    computerSelection == gameOptions[1]
-  ) {
-    return "You loose! Paper beats rock.";
-  } else if (
-    playerSelection == gameOptions[0] &&
-    computerSelection == gameOptions[2]
-  ) {
-    return "You win! Rock beats scissors.";
+    // Rock
+    if (
+      playerSelection == gameOptions[0] &&
+      computerSelection == gameOptions[0]
+    ) {
+      return "Put!";
+    } else if (
+      playerSelection == gameOptions[0] &&
+      computerSelection == gameOptions[1]
+    ) {
+      return "You loose! Paper beats rock.";
+    } else if (
+      playerSelection == gameOptions[0] &&
+      computerSelection == gameOptions[2]
+    ) {
+      return "You win! Rock beats scissors.";
+    }
+
+    // Paper
+    else if (
+      playerSelection == gameOptions[1] &&
+      computerSelection == gameOptions[0]
+    ) {
+      return "You win! Paper beats rock.";
+    } else if (
+      playerSelection == gameOptions[1] &&
+      computerSelection == gameOptions[1]
+    ) {
+      return "Put!";
+    } else if (
+      playerSelection == gameOptions[1] &&
+      computerSelection == gameOptions[2]
+    ) {
+      return "You loose! Scissors beats paper.";
+    }
+
+    // Scissors
+    else if (
+      playerSelection == gameOptions[2] &&
+      computerSelection == gameOptions[0]
+    ) {
+      return "You loose! Rock beats scissors.";
+    } else if (
+      playerSelection == gameOptions[2] &&
+      computerSelection == gameOptions[1]
+    ) {
+      return "You win! Scissors beats paper.";
+    } else if (
+      playerSelection == gameOptions[2] &&
+      computerSelection == gameOptions[2]
+    ) {
+      return "Put!";
+    }
+
+    return "No case matched";
+  } else {
+    return "Invalid game option";
   }
-
-  // PAPER
-  else if (
-    playerSelection == gameOptions[1] &&
-    computerSelection == gameOptions[0]
-  ) {
-    return "You win! Paper beats rock.";
-  } else if (
-    playerSelection == gameOptions[1] &&
-    computerSelection == gameOptions[1]
-  ) {
-    return "Put!";
-  } else if (
-    playerSelection == gameOptions[1] &&
-    computerSelection == gameOptions[2]
-  ) {
-    return "You loose! Scissors beats paper.";
-  }
-
-  // SCISSORS
-  else if (
-    playerSelection == gameOptions[2] &&
-    computerSelection == gameOptions[0]
-  ) {
-    return "You loose! Rock beats scissors.";
-  } else if (
-    playerSelection == gameOptions[2] &&
-    computerSelection == gameOptions[1]
-  ) {
-    return "You win! Scissors beats paper.";
-  } else if (
-    playerSelection == gameOptions[2] &&
-    computerSelection == gameOptions[2]
-  ) {
-    return "Put!";
-  }
-
-  return "No case matched";
 }
 
-console.log(gamePlay(computerPlay(), computerPlay()));
+function game(rounds) {
+  for (i = rounds; i > 0; i--) {
+    let playerSelection = prompt("Pick a rock, paper or scissors!");
+    console.log(gamePlay(playerSelection, computerPlay()));
+    console.log("===");
+  }
+}
+
+game(5);
+
+// console.log(gamePlay(computerPlay(), computerPlay()));
